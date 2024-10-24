b1b2b1ea5fcc72af86ad2c2e0eafaa081db6bb6f
firebladed <34522909+firebladed@users.noreply.github.com>
2024-10-24 10:59:57 +0100

    Update control.lua
    Altered to load under Factorio 2.0

--- control.lua
+++ control.lua
@@ -2,14 +2,14 @@
 --
 -- Licensed under MS-RL, see https://opensource.org/licenses/MS-RL
 
-if not global.logistic_signals then
-    global.logistic_signals = {};
+if not storage.logistic_signals then
+    storage.logistic_signals = {};
 end
 
 local function onEntityCreated(event)
-    if (event.created_entity.valid and (event.created_entity.name == "sil-unfulfilled-requests-combinator" or event.created_entity.name == "sil-player-requests-combinator")) then
-        event.created_entity.operable = false;
-        table.insert(global.logistic_signals, event.created_entity);
+    if (event.entity.valid and (event.entity.name == "sil-unfulfilled-requests-combinator" or event.entity.name == "sil-player-requests-combinator")) then
+        event.entity.operable = false;
+        table.insert(storage.logistic_signals, event.entity);
     end
 end
 
@@ -23,9 +23,9 @@ local function onEntityDeleted(event)
     local destroyed_unit_name = event.entity.name;
     if (destroyed_unit_name == "sil-unfulfilled-requests-combinator" or destroyed_unit_name == "sil-player-requests-combinator") then
         local removed_from_tracking = false;
-        for i = #global.logistic_signals, 1, -1 do
-            if (not global.logistic_signals[i].valid or global.logistic_signals[i].unit_number == destroyed_unit_number) then
-                table.remove(global.logistic_signals, i);
+        for i = #storage.logistic_signals, 1, -1 do
+            if (not storage.logistic_signals[i].valid or storage.logistic_signals[i].unit_number == destroyed_unit_number) then
+                table.remove(storage.logistic_signals, i);
                 removed_from_tracking = true;
             end
         end
@@ -39,41 +39,58 @@ local function processRequests(req, requests)
     local log_point = req.get_logistic_point(defines.logistic_member_index.logistic_container);
     if (not (log_point and log_point.valid)) then
         return
-    end
+    end	
     local buffer_chests_enabled = settings.global["sil-enable-buffer-chests"].value
     if (not ((log_point.mode == defines.logistic_mode.buffer and buffer_chests_enabled) or log_point.mode == defines.logistic_mode.requester)) then
         return
     end
-    for i = 1, req.request_slot_count do
-        local stack = req.get_request_slot(i);
-        if (stack ~= nil) then
-            local invIndex = defines.inventory.chest;
-            if (req.type == 'spider-vehicle') then
-                invIndex = defines.inventory.spider_ammo;
-            end
-            local inv = req.get_inventory(invIndex);
-            local curCount = 0;
-            if (inv == nil) then
-                log("Failed to get inventory of " .. req.name .. " a " .. req.type);
-            else
-                curCount = inv.get_item_count(stack.name);
-            end
-            if (req.type == 'spider-vehicle') then
-                inv = req.get_inventory(defines.inventory.spider_trunk);
-                if (inv ~= nil) then
-                    curCount = curCount + inv.get_item_count(stack.name);
-                end -- has Spider Trunk
-            end -- is SpiderTron
-            local needed = stack.count - curCount;
-            if (needed > 0) then
-                if (requests[stack.name]) then
-                    requests[stack.name] = requests[stack.name] + needed;
-                else
-                    requests[stack.name] = needed;
-                end
-            end -- only if items are missing, not deducting over-delivery by bots!
-        end -- is request slot used
-    end -- request slot count looping chest requests
+	
+	local requester_point = req.get_requester_point() 
+	
+	if requester_point == nil then
+		return
+	end	
+	
+	for j = 1, requester_point.sections_count do
+		local section = requester_point.sections[j]
+		
+		
+		log("Requester Type")
+		log(serpent.block(req.type)) 
+		for i = 1, section.filters_count do
+			local filter = section.filters[i];
+			log(serpent.block(filter))  
+			if (filter ~= nil) then
+				local invIndex = defines.inventory.chest;
+				if (req.type == 'spider-vehicle') then
+					invIndex = defines.inventory.spider_ammo;
+				end
+				local inv = req.get_inventory(invIndex);
+				local curCount = 0;
+				if (inv == nil) then
+					log("Failed to get inventory of " .. req.name .. " a " .. req.type);
+				else
+					curCount = inv.get_item_count(filter.name);
+				end
+				if (req.type == 'spider-vehicle') then
+					inv = req.get_inventory(defines.inventory.spider_trunk);
+					if (inv ~= nil) then
+						curCount = curCount + inv.get_item_count(filter.name);
+					end -- has Spider Trunk
+				end -- is SpiderTron
+				local needed = filter.min - curCount;
+				if (needed > 0) then
+					if (requests[filter.value.name]) then
+						requests[filter.value.name] = requests[filter.value.name] + needed;
+					else
+						requests[filter.value.name] = needed;
+					end
+				end -- only if items are missing, not deducting over-delivery by bots!
+			end -- is request slot used
+		end -- request slot count looping chest requests
+
+	end
+
     for item, count in pairs(log_point.targeted_items_pickup) do
         -- Add stuff that is about to be gone to the requested list
         if (requests[item]) then
@@ -142,37 +159,67 @@ local function processCombinator(obj)
         if ((req.type ~= "character" and obj.name == "sil-unfulfilled-requests-combinator") or (req.type == "character" and obj.name == "sil-player-requests-combinator")) then
             processRequests(req, requests);
         end
-    end -- requesters
-    local maxSignalCount = obj.get_control_behavior().signals_count;
+	--	if ((req.type == "item-request-proxy") or (req.type == "entity-ghost")) then
+    --        processRequests(req, requests);
+    --    end
+
+	end -- requesters
+    --local maxSignalCount = obj.get_control_behavior().signals_count;
     local ignored = 0;
     local signalIndex = 1;
     local MAX_VALUE = 2147483647; -- ((2 ^ 32 - 1) << 1) >> 1;
+
+	outsection = obj.get_or_create_control_behavior().get_section(1)
+	
+	if outsection == nil then
+		outsection = out.add_section()
+	end
+
+	outsection.filters = {}
+
     for k,v in pairs(requests) do
         if (v > 0) then
             if (v > MAX_VALUE) then
                 log('Value for ' .. k .. ' (' .. v .. ') exceeds 32 bit limit, clamping');
                 v = MAX_VALUE;
             end
-            local slot = { signal = { type = "item", name = k}, count = v, index = signalIndex};
-            if (signalIndex <= maxSignalCount) then
+            --local slot = { signal = { type = "item", name = k}, count = v, index = signalIndex};
+--            if (signalIndex <= maxSignalCount) then
                 signalIndex = signalIndex + 1;
-                table.insert(params, slot);
-            else
-                ignored = ignored + 1;
-            end
+
+				local logisticfilter = {}
+				local signalfilter = {}		
+
+				signalfilter.type = "item"
+				signalfilter.name = k
+				signalfilter.quality = "normal" 
+				signalfilter.comparator = nil	
+				
+				logisticfilter.value = signalfilter
+				logisticfilter.min = v
+				logisticfilter.max = v		
+					
+				outsection.set_slot(signalIndex,logisticfilter)
+
+ --           else
+ --               ignored = ignored + 1;
+ --           end
         end
     end
+	
     if (ignored > 0) then
         log('Ignored ' .. ignored .. ' requests, which exceed maximum number of requests');
     end
-    obj.get_or_create_control_behavior().parameters = params;
+	
+	outsection.active = true
+	
 end
 
 script.on_event({defines.events.on_pre_player_mined_item, defines.events.on_robot_pre_mined, defines.events.on_entity_died}, onEntityDeleted);
 script.on_event({defines.events.on_built_entity, defines.events.on_robot_built_entity}, onEntityCreated);
 
 script.on_nth_tick(30, function(event)
-    for _, obj in pairs(global.logistic_signals) do
+    for _, obj in pairs(storage.logistic_signals) do
         processCombinator(obj);
     end
 end);
